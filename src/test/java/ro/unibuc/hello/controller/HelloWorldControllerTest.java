31ea1c14bcf77c7999cba80ca0288ec00bae0cd5
Valentin Achimescu <50949589+Achime11@users.noreply.github.com>
2022-03-31 20:12:46 +0300

    Added HelloWorldService

---
+++ src/test/java/ro/unibuc/hello/controller/HelloWorldControllerTest.java
@@ -0,0 +1,10 @@
+package ro.unibuc.hello.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class HelloWorldControllerTest {
+
+}
